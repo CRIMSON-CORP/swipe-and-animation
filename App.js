4e91f791ad0e4bcc39335a62a4c8e39bcc4b72a2
CRIMSON <olueatoworosanwo@gmail.com>
2022-02-12 20:13:31 +0100

    react-native navigation tuts

--- App.js
+++ App.js
@@ -1,10 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
     return (
-        <View>
-            <Text>Plain Code</Text>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={({ route }) => ({
+                        title: route.params.homeText || "Home Screen",
+                        headerStyle: {
+                            backgroundColor: "#f4511e",
+                        },
+                        headerTintColor: "#fff",
+                        headerTitleStyle: {
+                            fontWeight: "bold",
+                        },
+                        headerLeft: () => <Button title="Go Back" color="blue"  />,
+                    })}
+                    initialParams={{
+                        homeText: "Inital text from parent",
+                    }}
+                />
+                <Stack.Screen
+                    name="AnotherScreen"
+                    component={AnotherScreen}
+                    options={{ title: "Another Screen Side" }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text
+                onPress={() =>
+                    navigation.navigate("AnotherScreen", {
+                        homeText: "Hello from Home",
+                    })
+                }
+            >
+                HomeScreen
+            </Text>
+            <Text onPress={() => navigation.setOptions({ title: "Set Title" })}>Set stuff</Text>
+        </View>
+    );
+}
+function AnotherScreen({ navigation, route }) {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text onPress={() => navigation.navigate("Home")}>AnotherScreen</Text>
+            <Text>{route.params && route.params.homeText}</Text>
         </View>
     );
 }
